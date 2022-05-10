8d50c139d54d89b5a8a55cb2027b341ae9015eca
Massimo Manzato <massimo.manzato@gmail.com>
2022-05-10 15:23:27 +0200

    shellcheck clean and optional root crypt

--- install.sh
+++ install.sh
@@ -50,18 +50,18 @@ HOME=""
 DATA=""
 
 yes_no_prompt(){
-    read -p "$1 [y/N] "
+    read -r -p "$1 [y/N] "
 }
 
 contains_element(){
-    for e in "${@:2}"; do [[ $e == $1 ]] && break; done;
+    for e in "${@:2}"; do [[ $e == "$1" ]] && break; done;
 }
 
 select_partition(){
-    partitions_list=`lsblk | grep 'part' | awk '{print "/dev/" substr($1,3)}'`;
+    partitions_list=$(lsblk | grep 'part' | awk '{print "/dev/" substr($1,3)}');
     PS3="$1: "
     select partition in $partitions_list; do
-        if contains_element $partition; then
+        if contains_element "$partition"; then
             yes_no_prompt "Is $partition the correct partition:"
             if [[ $REPLY == "y" ]]; then
                 PARTITION=$partition
@@ -73,41 +73,47 @@ select_partition(){
 
 config_disks(){
     printf "Root disk: %s \n" "$RDISK" 
-    cfdisk $RDISK
+    cfdisk "$RDISK"
 
     if [[ $DDISK != "none" ]]; then
         printf "Data  disk: %s \n" "$DDISK" 
-        cfdisk $DDISK
+        cfdisk "$DDISK"
     fi
 }
 
 setup_luks(){
     select_partition "Select root partition"
 
-    echo "Encrypting ${PARTITION}..."
-    cryptsetup luksFormat $PARTITION
-    cryptsetup open $PARTITION $ROOTLUKS
+    if [[ $LUKSROOT -eq 1 ]]; then
+    	echo "Encrypting ${PARTITION}..."
+    	cryptsetup luksFormat "$PARTITION"
+    	cryptsetup open "$PARTITION" "$ROOTLUKS"
+    	ROOT=/dev/mapper/$ROOTLUKS
+    else 
+	ROOT=$PARTITION
+    fi
 
     if [[ $DDISK != "none" ]]; then
         select_partition "Select data partition"
 
         dd if=/dev/random of=data.key bs=1 count=32
 
-        cryptsetup luksFormat $PARTITION data.key
-        cryptsetup open $PARTITION $DATALUKS --key-file data.key
+        cryptsetup luksFormat "$PARTITION" data.key
+        cryptsetup open "$PARTITION" "$DATALUKS" --key-file data.key
 
         DATA=/dev/mapper/$DATALUKS
     fi
-
-    ROOT=/dev/mapper/$ROOTLUKS
+    
+    #to be removed if work LUKSROOT
+    #ROOT=/dev/mapper/$ROOTLUKS
 }
 
 mount_filesytems(){
-    mount -o $BTRFS_OPTS,subvol=@ $ROOT /mnt
+    mount -o "$BTRFS_OPTS",subvol=@ "$ROOT" /mnt
 
     mkdir -p /mnt/{boot,dev,proc,sys,home,mnt,var}
 
-    mount -o $BTRFS_OPTS,subvol=@home $ROOT /mnt/home
+    mount -o "$BTRFS_OPTS",subvol=@home "$ROOT" /mnt/home
 
     # create seperate subvolumes for log, cache, and tmp to prevent them
     # from being in snapshots of the root subvolume
@@ -115,7 +121,7 @@ mount_filesytems(){
     btrfs subvolume create /mnt/var/cache
     btrfs subvolume create /mnt/var/tmp
 
-    mount $BOOT /mnt/boot
+    mount "$BOOT" /mnt/boot
 
     mount --rbind /dev/ /mnt/dev
     mount --rbind /proc/ /mnt/proc
@@ -127,8 +133,8 @@ mount_filesytems(){
         mv data.key /mnt/root
 
         mkdir -p /mnt/mnt/{vault,snapshots}
-        mount -o $BTRFS_OPTS,subvol=@vault $DATA /mnt/mnt/vault
-        mount -o $BTRFS_OPTS,subvol=@snapshots $DATA /mnt/mnt/snapshots
+        mount -o $BTRFS_OPTS,subvol=@vault "$DATA" /mnt/mnt/vault
+        mount -o $BTRFS_OPTS,subvol=@snapshots "$DATA" /mnt/mnt/snapshots
 
         btrfs subvolume create /mnt/mnt/vault/storage
         btrfs subvolume create /mnt/mnt/vault/vms
@@ -136,17 +142,17 @@ mount_filesytems(){
 }
 
 setup_btrfs(){
-    mkfs.btrfs -L root -d single -m single $ROOT
+    mkfs.btrfs -L root -d single -m single "$ROOT"
 
-    mount -o $BTRFS_OPTS $ROOT /mnt
+    mount -o "$BTRFS_OPTS" "$ROOT" /mnt
     btrfs subvolume create /mnt/@
     btrfs subvolume create /mnt/@home
     umount /mnt
 
     if [[ $DDISK != "none" ]]; then
-        mkfs.btrfs -L data -d single -m dup $DATA
+        mkfs.btrfs -L data -d single -m dup "$DATA"
 
-        mount -o $BTRFS_OPTS $DATA /mnt
+        mount -o "$BTRFS_OPTS" "$DATA" /mnt
         btrfs subvolume create /mnt/@vault
         btrfs subvolume create /mnt/@snapshots
         umount /mnt
@@ -166,10 +172,10 @@ boot_partition(){
                 ;;
             "y")
                 echo "Going to create a boot partition."
-                cfdisk $RDISK
+                cfdisk "$RDISK"
                 select_partition "Select boot partition"
-                BOOT=$PARTITION
-                mkfs.vfat -F 32 $BOOT
+                BOOT="$PARTITION"
+                mkfs.vfat -F 32 "$BOOT"
                 break
                 ;;
             *) echo Invalid;;
@@ -183,7 +189,7 @@ bootstrap(){
         repo="$REPO/current/musl/"
     fi
 
-    xbps-install -Sy -R $repo -r /mnt base-system btrfs-progs cryptsetup ntp refind vim
+    xbps-install -Sy -R "$repo" -r /mnt base-system btrfs-progs cryptsetup ntp refind vim
     xbps-reconfigure -r /mnt -f base-files
     chroot /mnt xbps-reconfigure -a
 }
@@ -215,7 +221,7 @@ bootstrap
 cp ./chroot.sh /mnt/
 cp ./config.sh /mnt/
 
-chroot /mnt ./chroot.sh $RDISK $ROOT $BOOT $DATA $USR
+chroot /mnt ./chroot.sh "$RDISK" "$ROOT" "$BOOT" "$DATA" "$USR"
 
 # cleanup
 rm /mnt/chroot.sh /mnt/config.sh
